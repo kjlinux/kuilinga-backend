cf4166b370809c99bc597c4cc570dcebb91fe93f
Ghost <koffijude33@gmail.com>
2025-10-17 13:38:33 +0000

    update

--- app/schemas/report.py
+++ app/schemas/report.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from uuid import UUID
-from typing import Optional, List
+from typing import Optional, List, Dict, Any
 from datetime import date
 from enum import Enum
 
